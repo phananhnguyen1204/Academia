1745f7b8f42536a6c6c06e2ac6486dfa39548cc7
Phan Anh <phananh12042003@gmail.com>
2023-12-22 20:53:42 -0500

    teacher routes 🚀

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -15,11 +15,14 @@ export const NavbarRoutes = () => {
   return (
     <div className="flex gap-x-2 ml-auto">
       {isTeacherPage || isPlayerPage ? (
-        <Button size="sm" variant="ghost">
-          <LogOut className="h-4 w-4 mr-2">Exit</LogOut>
-        </Button>
+        <Link href="/">
+          <Button size="sm" variant="ghost">
+            <LogOut className="h-4 w-4 mr-2"></LogOut>
+            Exit
+          </Button>
+        </Link>
       ) : (
-        <Link href="/teacher/course">
+        <Link href="/teacher/courses">
           <Button size="sm" variant="ghost">
             Teacher Mode
           </Button>
