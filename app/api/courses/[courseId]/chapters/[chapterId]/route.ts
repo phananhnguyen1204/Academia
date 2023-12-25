2cd7d00c15230d37a1c974547db7b1cf4c2a7924
Phan Anh <phananh12042003@gmail.com>
2023-12-25 03:21:04 -0500

    chapter video form 🚀

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,7 +1,13 @@
+import Mux from "@mux/mux-node";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
+
+const { Video } = new Mux(
+  process.env.MUX_TOKEN_ID!,
+  process.env.MUX_TOKEN_SECRET!
+);
 
 export async function DELETE(
   req: Request,
@@ -36,22 +42,22 @@ export async function DELETE(
       return new NextResponse("Not Found", { status: 404 });
     }
 
-    // if (chapter.videoUrl) {
-    //   const existingMuxData = await db.muxData.findFirst({
-    //     where: {
-    //       chapterId: params.chapterId,
-    //     },
-    //   });
+    if (chapter.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
 
-    //   if (existingMuxData) {
-    //     await Video.Assets.del(existingMuxData.assetId);
-    //     await db.muxData.delete({
-    //       where: {
-    //         id: existingMuxData.id,
-    //       },
-    //     });
-    //   }
-    // }
+      if (existingMuxData) {
+        await Video.Assets.del(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
+    }
 
     const deletedChapter = await db.chapter.delete({
       where: {
@@ -117,36 +123,36 @@ export async function PATCH(
       },
     });
 
-    // if (values.videoUrl) {
-    //   const existingMuxData = await db.muxData.findFirst({
-    //     where: {
-    //       chapterId: params.chapterId,
-    //     },
-    //   });
+    if (values.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
 
-    //   if (existingMuxData) {
-    //     await Video.Assets.del(existingMuxData.assetId);
-    //     await db.muxData.delete({
-    //       where: {
-    //         id: existingMuxData.id,
-    //       },
-    //     });
-    //   }
+      if (existingMuxData) {
+        await Video.Assets.del(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
 
-    // const asset = await Video.Assets.create({
-    //   input: values.videoUrl,
-    //   playback_policy: "public",
-    //   test: false,
-    // });
+      const asset = await Video.Assets.create({
+        input: values.videoUrl,
+        playback_policy: "public",
+        test: false,
+      });
 
-    //   await db.muxData.create({
-    //     data: {
-    //       chapterId: params.chapterId,
-    //       assetId: asset.id,
-    //       playbackId: asset.playback_ids?.[0]?.id,
-    //     },
-    //   });
-    // }
+      await db.muxData.create({
+        data: {
+          chapterId: params.chapterId,
+          assetId: asset.id,
+          playbackId: asset.playback_ids?.[0]?.id,
+        },
+      });
+    }
 
     return NextResponse.json(chapter);
   } catch (error) {
